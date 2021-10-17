958e9784ba1aba84fe6c990178017238aa77bf46
Hujanja <arman11102@mail.ru>
2021-10-18 00:07:14 +0300

    Background color and other

--- PewPewGame/GameScene.swift
+++ PewPewGame/GameScene.swift
@@ -10,5 +10,50 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+
     
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        
+        
+        setScene(view)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        for touch in touches {
+            if touch == touches.first {
+                print("Touched")
+            }
+        }
+    }
+    
+    func setScene(_ view: SKView) {
+        backgroundColor = colors.darkBlue
+        addButton()
+        
+    }
+
+    func addButton() {
+        let button = SKShapeNode(circleOfRadius: 35)
+        button.fillColor = colors.pink
+        button.strokeColor = colors.pink
+        guard let view = view else {
+            return
+        }
+        button.position = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
+        addChild(button)
+    }
+    
+    
+    
+    struct colors {
+        static let darkBlue = UIColor(red: 16/255, green: 19/255, blue: 58/255, alpha: 1)
+        static let orange = UIColor(red: 246/255, green: 135/255, blue: 65/255, alpha: 1)
+        static let pink = UIColor(red: 239/255, green: 1/255, blue: 149/255, alpha: 1)
+    }
+
 }
+
--- PewPewGame/GameViewController.swift
+++ PewPewGame/GameViewController.swift
@@ -11,5 +11,14 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.frame.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.presentScene(scene)
+    }
 }
+
